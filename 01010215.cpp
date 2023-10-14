28d9dc0520f59f1f199d42657364cc5dbbcc5095
Dlmitriyyy <streltsowdm@gmail.com>
2023-10-14 17:46:51 +0300

    Добавьте файлы проекта.

---
+++ 01010215.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+	system("chcp 1251 > nul");
+
+	double verst, km;
+
+	cout << "Введіть кількість верст: ";
+	cin >> verst;
+
+	km = verst * 1.067;
+
+	cout << fixed << setprecision(3) << km << "Км";
+	return 0;
+}
